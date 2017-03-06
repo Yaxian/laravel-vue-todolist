0dd1adb419432dc3cec9934d583e9e03513c3acc
kckct <ss010359@gmail.com>
2017-03-06 21:11:41 +0800

    加入新增及列表功能

---
+++ resources/assets/js/components/Tasks.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <div class="row"></div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Todo List</div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                                <input type="text" v-model="task.body" name="body" class="form-control" placeholder="新增要做的事項..." @keyup.enter="createTask()" autofocus>
+                        </div>
+                        <div class="form-group">
+                                <ul class="list-group">
+                                    <li class="list-group-item" v-for="task in tasks">
+                                        {{ task.body }}
+                                    </li>
+                                </ul>
+                        </div>
+                        <!-- {{ JSON.stringify($data) }} -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                tasks: [
+                    //{'body': '1'},
+                    //{'body': '2'},
+                ],
+                task: {
+                    id: '',
+                    body: ''
+                }
+            }
+        },
+
+        mounted: function () {
+            this.getAllTasks();
+        },
+
+        methods: {
+            getAllTasks: function () {
+                this.$http.get('api/tasks').then(function (response) {
+                    this.tasks = response.data;
+                });
+            },
+
+            createTask: function () {
+                this.$http.post('api/task/store', this.task);
+                this.task.body = '';
+                this.getAllTasks();
+            }
+        }
+    }
+</script>
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api'], function () {
+    Route::get('tasks', 'TaskController@getAllTasks');
+    Route::post('task/store', 'TaskController@store');
+});
