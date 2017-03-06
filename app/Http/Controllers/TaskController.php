b3a0c90b1319ea26ca89f3ad4298c2e98ff3fb81
kckct <ss010359@gmail.com>
2017-03-06 15:39:29 +0800

    TaskController加入CRUD功能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,10 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Task;
+
 class TaskController extends Controller
 {
     public function index()
     {
         return view('task');
+    }
+
+    public function getAllTasks()
+    {
+        return Task::latest()->orderBy('created_at', 'desc')->get();
+    }
+
+    public function getTask($id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->input(['body']);
+        return Task::create(['body' => $data]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->input(['body']);
+        return Task::findOrFail($id)->update(['body' => $data]);
+    }
+
+    public function destroy($id)
+    {
+        return Task::destroy($id);
     }
 }
