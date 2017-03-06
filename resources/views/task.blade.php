40a8ab2bbc55c659e7ada2bc257fae8f2be48d66
kckct <ss010359@gmail.com>
2017-03-06 21:06:22 +0800

    修正task.blade.php, javascript加入csrfToken

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -6,15 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="_token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Todo List') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
 </head>
 <body>
-    <div class="container">
+    <div id="app">
         <tasks></tasks>
     </div>
 
